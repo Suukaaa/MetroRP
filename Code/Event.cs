f466396b71f541977d3185576b0fc22e73746bdb
Suukaaa <cheh.corporation@gmail.com>
2026-01-25 18:19:20 +0100

    Update Event.cs

--- Code/Event.cs
+++ Code/Event.cs
@@ -6,9 +6,32 @@ public sealed class Event : Component, PlayerController.IEvents
 	[Property]
 	public PlayerController MyController { get; set; }
 
+	void PlayerController.IEvents.OnEyeAngles( ref Angles angles )
+	{
+		Log.Info("Angles");
+	}
+	void PlayerController.IEvents.PostCameraSetup( CameraComponent cam )
+	{
+		Log.Info("PostCameraEvents");
+	}
+	void PlayerController.IEvents.OnJumped()
+	{
+		Log.Info("Jump");
+	}
 	void PlayerController.IEvents.OnLanded( float distance, Vector3 impactVelocity )
 	{
 		Log.Info("Land");
-		PlayerController.Body;
+	}
+	void PlayerController.IEvents.StartPressing( Component target )
+	{
+		Log.Info("StartPress");
+	}
+	void PlayerController.IEvents.StopPressing( Component target )
+	{
+		Log.Info("StopPress");
+	}
+	void PlayerController.IEvents.FailPressing()
+	{
+		Log.Info("FailPress");
 	}
 }
