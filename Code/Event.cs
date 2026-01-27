5ae2ed4bd0db07340e0794d571fc9c51e1eca663
iSowDeepp <seach.romain56@gmail.com>
2026-01-27 21:26:32 +0100

    Avancé UI et event

--- Code/Event.cs
+++ Code/Event.cs
@@ -2,35 +2,30 @@ using System.Dynamic;
 using System.Net.Mail;
 using Microsoft.VisualBasic;
 using Sandbox;
+using Sandbox.Audio;
 
 public sealed class Event : Component, PlayerController.IEvents
 {
 
 	[Property] public PlayerController MyController { get; set; }
 
-	private GameObject parent;
 	private PlayerParam playerParam;
 
+		protected override void OnUpdate()
+	{
+		Log.Info ("coucou");
+	}
 	protected override void OnStart()
 	{
-		parent = GameObject;
-		playerParam = parent.Components.Get<PlayerParam>();
+		
+		playerParam = GameObject.Components.Get<PlayerParam>();
 		playerParam.SetHealth(100);
 	}
-
-
-	void PlayerController.IEvents.OnEyeAngles( ref Angles angles )
-	{
-		// laissez en commentaires ça s'execute en boucles
-		//Log.Info("Angles");
-	}
-	void PlayerController.IEvents.PostCameraSetup( CameraComponent cam )
-	{
-		// laissez en commentaires ça s'execute en boucles
-		//Log.Info("PostCameraEvents");
-	}
+	
 	void PlayerController.IEvents.OnJumped()
 	{
-		playerParam.IncrementOxygen(80);
+
 	}
+
+
 }
