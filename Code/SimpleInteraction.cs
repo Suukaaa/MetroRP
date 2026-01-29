59e01478e2617d43f84f58a3b770c6173cb7763f
iSowDeepp <seach.romain56@gmail.com>
2026-01-29 21:26:02 +0100

    Revert "Merge branch 'main' of https://github.com/Suukaaa/MetroRP"
    This reverts commit da7c39cc2e47c9ab07e15e2f853857ea1af3c4ba, reversing
    changes made to 51abc6273205148dd877968b068feaee779e4bc4.

--- Code/SimpleInteraction.cs
+++
@@ -1,8 +0,0 @@
-using Sandbox;
-
-public class SimpleInteraction : Component
-{
-    [Property] public string InteractionName { get; set; } = "InteractionName";
-
-    [Property] public float HoldDuration { get; set; } = 0.0f;
-}
